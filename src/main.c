3a37fd584d32d82481e4ba815874a8409a9b50ff
jantar1154 <davidbednarek@email.cz>
2024-01-28 16:56:57 +0100

    Added possibility to open file from argument

--- src/main.c
+++ src/main.c
@@ -6,8 +6,6 @@
 #include "h/file_handler.h"
 #include "h/debt.h"
 
-#define FILE_NAME "debt.dat"
-
 /*
     FILE HIEARCHY
     two bytes: number of debts (entries) (ushort)
@@ -15,20 +13,22 @@
     every other entry is 148 bytes (sizeof(Debt))
 */
 
-int main() {
+int main(int argc, char ** argv) {
+    const char * filename = "debt.dat";
+    if (argc > 1) filename = argv[1];
     short quit = 0;
 
     char * command = malloc(0xFF * sizeof(char));
-    if (!fh_init_file(FILE_NAME)) fh_create_file(FILE_NAME);
-    Debt * debt_arr = malloc(sizeof(Debt) * fh_get_debt_count(FILE_NAME));
+    if (!fh_init_file(filename)) fh_create_file(filename);
+    Debt * debt_arr = malloc(sizeof(Debt) * fh_get_debt_count(filename));
 
     while (!quit) {
-        debt_arr = realloc(debt_arr, sizeof(Debt) * fh_get_debt_count(FILE_NAME));
+        debt_arr = realloc(debt_arr, sizeof(Debt) * fh_get_debt_count(filename));
         fprintf(stdout, "iou > ");
         fgets(command, 0xFF * sizeof(char), stdin);
         if (strlen(command) <= 1) continue;
         command = strtok(command, "\n");
-        i_handle_input(debt_arr, command, &quit, FILE_NAME);
+        i_handle_input(debt_arr, command, &quit, filename);
     }
     free(debt_arr);
     free(command);
