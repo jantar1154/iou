1d6dae479f18d5d00d63d28c9c8a81ad8a69d634
jantar1154 <davidbednarek@email.cz>
2024-02-09 08:44:37 +0100

    Improved printing out data

--- src/display.c
+++ src/display.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "h/display.h"
 
+#define LINE_W 24
+
+// Prints a line
+void d_print_line() {
+    for (int i = 0; i < LINE_W; ++i) {
+        putchar('-');
+    }
+    putchar('\n');
+}
+
 // Prints out every debt from `debt_arr`
 void d_print_debts(Debt * debt_arr, int debt_count) {
-    putchar('\n');
     for (int i = 0; i < debt_count; i++) {
         Debt * d = debt_arr + i;
-        printf("[%i]\nFrom:\t%s\nTo:\t%s\nAmount:\t%i %s\n\n",
+        d_print_line();
+        printf("[%i]\nFrom:\t%s\nTo:\t%s\nAmount:\t%i %s\n",
             i+1, d->from, d->to, d->amount, d->currency
         );
     }
+    d_print_line();
 }
